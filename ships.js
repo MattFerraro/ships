b21716d3a34a186304cda5adc2e6a20e56f7ecbe
Matt Ferraro <matt@planet.com>
2015-10-18 16:12:54 -0700

    First pass at global state init and drawing

---
+++ ships.js
@@ -0,0 +1,96 @@
+var shipPrototypes = {
+	"default": {
+		"size": 10,
+		"maxSpeed": 1,
+		"initialHealth": 100
+	}
+}
+
+function globalInit() {
+	// Returns the initial global state
+	var globalState = {};
+
+	var colors = ["red", "blue"];
+	var basePositions = [{x: 100, y: 350}, {x: 600, y: 350}];
+	var numShips = 10;
+	globalState.teams = [];
+	for (var i = 0; i < 2; i++) {
+		var team = {};
+		team.color = colors[i];
+
+		team.base = {
+			position: basePositions[i],
+			size: 40
+		};
+
+		team.ships = [];
+		for (var j = 0; j < numShips; j++) {
+			var ship = {};
+			ship.class = "default";
+			// Copy over all the default params from the ship prototype
+			for(var k in shipPrototypes[ship.class]) {
+				ship[k]=shipPrototypes[ship.class][k];
+			}
+			ship.x = team.base.position.x;
+			ship.y = team.base.position.y + 20 * j - numShips/2 * 20;
+			ship.heading = 0;
+			ship.speed = 0;
+			ship.health = ship.initialHealth;
+			team.ships.push(ship);
+		}
+		globalState.teams.push(team);
+	}
+	return globalState;
+}
+
+
+function globalUpdate(globalState) {
+	// Input: the global state
+	// This function's job is to morph the global state to propogate the
+	// physics of the simulated universe
+}
+
+function updateTeamZero(globalState) {
+	// Input is (a copy of) the entire global state
+	// Output is a dictionary of commands that are addressed to the ships
+	// under the command of team zero
+}
+
+function updateTeamOne(globalState) {
+	// Input is (a copy of) the entire global state
+	// Output is a dictionary of commands that are addressed to the ships
+	// under the command of team one
+}
+
+function globalDraw(canvas, globalState) {
+	var ctx=canvas.getContext("2d");
+
+	// clear the screen
+	ctx.fillStyle="#000000";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// console.log(globalState);
+	// draw the teams
+	for (var i = globalState.teams.length - 1; i >= 0; i--) {
+		var team = globalState.teams[i];
+		ctx.fillStyle = team.color;
+
+		// draw the home base
+		var base = team.base;
+		ctx.fillRect(base.position.x - base.size / 2, team.base.position.y - base.size / 2, base.size, base.size);
+
+		// draw each ship
+		for (var j = team.ships.length - 1; j >= 0; j--) {
+			var ship = team.ships[j];
+			ctx.fillRect(ship.x - ship.size / 2, ship.y - ship.size / 2, ship.size, ship.size);
+
+			var headingY = Math.sin(ship.heading) * 10;
+			var headingX = Math.cos(ship.heading) * 10;
+			ctx.strokeStyle = "#FFFFFF";
+			ctx.beginPath();
+			ctx.moveTo(ship.x,ship.y);
+			ctx.lineTo(ship.x + headingX, ship.y + headingY);
+			ctx.stroke();
+		};
+	};
+}
