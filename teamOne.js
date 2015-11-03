1a0cbb0b66f6377470d3722220a37e06981ad50b
Matt Ferraro <matt@planet.com>
2015-11-03 00:01:06 -0800

    team one now surrounds the enemy base

--- teamOne.js
+++ teamOne.js
@@ -1,15 +1,68 @@
 function teamOneInit(globalState) {
     // Inputs: a copy of the global state
     // Outputs: team one's initial state, a dictionary
-    return {}
+
+    // Each ship should pick a random spot near the enemy base and position
+    // itself there. Pick our location by imagining a circle around the enemy
+    // base, then picking a random spot on the circumference
+    var radius = 100;
+    var enemyTeam = globalState.teams[0];
+    var myTeam = globalState.teams[1];
+    var extraShipVars = _.map(myTeam.ships, function(ship) {
+        var angle = Math.random() * Math.PI - Math.PI / 2;
+        var x = Math.cos(angle) * radius + enemyTeam.base.position.x;
+        var y = Math.sin(angle) * radius + enemyTeam.base.position.y;
+
+        return {
+            goalX: x,
+            goalY: y,
+            targetX: enemyTeam.base.position.x,
+            targetY: enemyTeam.base.position.y
+        };
+    });
+    return {
+        extraShipVars: extraShipVars
+    };
 }
 
-function getCommandsTeamOne(globalState, teamOneState) {
+function getCommandsTeamOne(globalState, teamState) {
     // Input is (a copy of) the entire global state
     // Output is a dictionary of commands that are addressed to the ships
     // under the command of team one (the blue team)
 
-    return _.map(globalState.teams[1].ships, function (ship) {
-        return {torque: 0.0, mainThrust: 10, sideThrust: 0}
-    });
+    var myTeam = globalState.teams[1];
+    var enemyTeam = globalState.teams[0];
+    var enemyBase = enemyTeam.base;
+    var extraShipVars = teamState.extraShipVars;
+
+    var commands = [];
+    for (var i = 0; i < myTeam.ships.length; i++) {
+        var ship = myTeam.ships[i];
+        var extraShipState = extraShipVars[i];
+        var torque = 0;
+        var mainThrust = 0;
+        var sideThrust = 0
+
+        var positionErrX = extraShipState.goalX - ship.x;
+        var positionErrY = extraShipState.goalY - ship.y;
+
+        var dt = 0.0001;
+
+        var positionErrXdt = ship.dx;
+        var positionErrYdt = ship.dy;
+
+        var Kp = -1;
+        var Kd = 1;
+
+        mainThrust = positionErrX * Kp + positionErrXdt * Kd;
+        // mainThrust = 0;
+        sideThrust = positionErrY * Kp + positionErrYdt * Kd;
+        // sideThrust = 0;
+        commands.push({
+            torque: torque,
+            mainThrust: mainThrust,
+            sideThrust: sideThrust
+        });
+    }
+    return commands;
 }
